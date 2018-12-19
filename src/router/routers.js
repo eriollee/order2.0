54ba6ddc8511f263ebed56bc65d64007aef09fda
eriollee <eriollee@163.com>
2018-12-19 17:24:16 +0800

    完成首页显示

--- src/router/routers.js
+++ src/router/routers.js
@@ -3,12 +3,24 @@ import Main from '@/components/main'
 export default [
     {
         path: '/',
-        name: 'login',
+        redirect: '/order/manager',
+        name: '_order_manager',
         meta: {
             icon: 'md-notifications',
             title: '订单统计'
         },
         component: Main,
+        children: [
+            {
+              path: 'order/manager',
+              name: 'order_manager',
+              meta: {
+                icon: 'md-notifications',
+                title: '订单统计',
+              },
+              component: () => import('@/view/order-manager/order-manager.vue')
+            }
+          ]
     },
     {
         path: '/order',
@@ -26,7 +38,7 @@ export default [
               title: '订单管理',
               beforeCloseName: 'before_close_normal'
             },
-            component: () => import('@/view/order-list.vue')
+            component: () => import('@/view/order-list/order-list.vue')
           }
         ]
       },
---
+++ src/view/order-manager/order-manager.vue
@@ -0,0 +1,18 @@
+<template>
+  <div >
+    order_manager
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'order_manager',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+</style>
